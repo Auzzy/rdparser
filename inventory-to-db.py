a6467aeeeebac033135bae09c140a0fe927e06c9
Austin Noto-Moniz <metalnut4@netscape.net>
2018-05-23 23:53:09 -0400

    Saving other important files.

---
+++ inventory-to-db.py
@@ -0,0 +1,37 @@
+import collections
+import json
+import os
+import sqlite3
+
+INVENTORY_DIR = "/home/auzzy/git/rdparser/inventory/"
+
+def load_inventory():
+    category_to_products = {}
+    for category_filename in os.listdir(INVENTORY_DIR):
+        category_name = os.path.splitext(category_filename)[0].replace("-", " ")
+        with open(os.path.join(INVENTORY_DIR, category_filename)) as category_file:
+            category_to_products[category_name] = [product.strip() for product in set(json.load(category_file))]
+
+    return category_to_products
+
+inventory = load_inventory()
+
+connection = sqlite3.connect("inventory.db")
+cursor = connection.cursor()
+
+cursor.execute("PRAGMA foreign_keys = ON")
+cursor.execute("CREATE TABLE IF NOT EXISTS categories (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, store TEXT, parentid INTEGER, FOREIGN KEY(parentid) REFERENCES categories(id))")
+cursor.execute("""CREATE TABLE IF NOT EXISTS products (name, categoryid, url, store, stocked)""")
+
+store = "Restaurant Depot"
+cursor.execute("INSERT INTO categories (name, store) VALUES (?, ?)", (store, store))
+root_category_id = cursor.lastrowid
+for category, products in inventory.items():
+    cursor.execute("INSERT INTO categories (name, store, parentid) VALUES (?, ?, ?)", (category, store, root_category_id))
+    category_id = cursor.lastrowid
+    for product in products:
+        cursor.execute("INSERT INTO products (name, categoryid, url, store, stocked) VALUES (?, ?, ?, ?, ?)", (product, category_id, None, store, None))
+
+
+connection.commit()
+connection.close()
