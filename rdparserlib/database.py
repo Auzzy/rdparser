a769adba8435765b91a92e15ac05f82bd96365ab
Austin <mathfreak65@gmail.com>
2020-02-18 12:38:38 +0000

    Restructure inventory to store more info.
    The inventory is now a list of JSON objects. Each object currently
    contains the product's name and category. The category field is
    structured to allow it to encode a single product in multiple
    categories, and each category to have subcategories. While RD currently
    has neither of these features, it brings it in line with my other store
    inventory file. Also it's not unreasonable for RD to add this later.

--- rdparserlib/database.py
+++ rdparserlib/database.py
@@ -12,13 +12,46 @@ def _db_disconnect(cursor, connection):
     connection.commit()
     connection.close()
 
+def _insert_if_new(cursor, table_name, data):
+    colnames = list(data.keys())
+    coldata = [data[colname] for colname in colnames]
+
+    sql_where_format = " AND ".join(["{} = ?".format(colname) for colname in colnames])
+    sql_where = "SELECT id FROM {} WHERE {}".format(table_name, sql_where_format)
+    cursor.execute(sql_where, coldata)
+
+    category_rows = cursor.fetchall()
+    if not category_rows:
+        sql_insert_format = ", ".join(colnames)
+        sql_insert_values_format = ", ".join(len(colnames) * "?")
+        sql_insert = "INSERT INTO {} ({}) VALUES ({})".format(table_name, sql_insert_format, sql_insert_values_format)
+        cursor.execute(sql_insert, coldata)
+        return cursor.lastrowid
+    elif len(category_rows) == 1:
+        return category_rows[0][0]
+    else:
+        raise Exception("There are multiple rows matching {}".format(data))
+
+def _insert_categories(cursor, categories, store):
+    if categories:
+        category = max(categories, key=len)
+    else:
+        return None
+
+    parent_id = _insert_if_new(cursor, "categories", {"name": category[0], "store": store})
+    for subcategory in category[1:]:
+        parent_id = _insert_if_new(cursor, "categories", {"name": subcategory, "store": store, "parentid": parent_id})
+    return parent_id
+
 def _insert_inventory(cursor, inventory):
     store = "Restaurant Depot"
-    for category, products in inventory.items():
-        cursor.execute("INSERT INTO categories (name, store) VALUES (?, ?)", (category, store))
-        category_id = cursor.lastrowid
-        for product in products:
-            cursor.execute("INSERT INTO products (name, categoryid, url, store, stocked) VALUES (?, ?, ?, ?, ?)", (product, category_id, None, store, None))
+    for item in inventory["inventory"]:
+        category_id = _insert_categories(cursor, item["categories"], store)
+        if not category_id:
+            print("No category information for {}".format(item["name"]))
+            continue
+
+        cursor.execute("INSERT INTO products (name, categoryid, url, store, stocked) VALUES (?, ?, ?, ?, ?)", (item["name"], category_id, None, store, None))
 
 def _create_tables(cursor):
     cursor.execute("PRAGMA foreign_keys = ON")
