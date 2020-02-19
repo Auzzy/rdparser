2ddc2b54c5a5ff3579070e171531840918fd921c
Austin <mathfreak65@gmail.com>
2020-02-19 12:57:05 +0000

    Remove database output.

--- bin/rdparser.py
+++ bin/rdparser.py
@@ -1,14 +1,13 @@
 import argparse
 
-from rdparserlib import download_and_populate
+from rdparserlib import download
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("database_filepath", help="Path to a database file to dump the inventory. If the file doesn't exist, it will be created.")
-    parser.add_argument("--inventory-filepath", help="Path to a file to store the inventory as JSON. This allows caching, as well as rebuilding the database.")
+    parser.add_argument("inventory_filepath", help="Path to a file to store the inventory as JSON.")
     return vars(parser.parse_args())
 
 if __name__ == "__main__":
     args = parse_args()
 
-    download_and_populate(args.get("inventory_filepath"), args["database_filepath"])
+    download(args["inventory_filepath"])
--- rdparserlib/__init__.py
+++ rdparserlib/__init__.py
@@ -1,6 +1,1 @@
 from rdparserlib.inventory import download
-from rdparserlib.database import populate
-
-def download_and_populate(inventory_filepath, database_filepath):
-    inventory = download(inventory_filepath)
-    populate(inventory, database_filepath)
